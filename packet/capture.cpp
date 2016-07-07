3bf8a0001a2a89db54b8df71f8dccc02f12b5ec3
holic159 <qkrdmltjd1234@gmail.com>
2016-07-08 00:18:19 +0900

    add protocol tcp filter

--- packet/capture.cpp
+++ packet/capture.cpp
@@ -12,6 +12,7 @@ void packetHandler(u_char *userData, const struct pcap_pkthdr* pkthdr, const u_c
   char source_mac[20], destination_mac[20];
   unsigned short ethernet_type;
   char ip_version;
+  char protocol;
   long source_ip_long, destination_ip_long;
   unsigned short source_port, destination_port;
 
@@ -19,38 +20,40 @@ void packetHandler(u_char *userData, const struct pcap_pkthdr* pkthdr, const u_c
 
   sprintf(destination_mac, "%02x:%02x:%02x:%02x:%02x:%02x", \
           (void*)packet[0], (void*)packet[1], (void*)packet[2], \
-          (void*)packet[3], (void*)packet[4], (void*)packet[5]);                 // get Destination MAC Address
+          (void*)packet[3], (void*)packet[4], (void*)packet[5]);                     // get Destination MAC Address
   sprintf(source_mac, "%02x:%02x:%02x:%02x:%02x:%02x", \
           (void*)packet[6], (void*)packet[7], (void*)packet[8], \
-          (void*)packet[9], (void*)packet[10], (void*)packet[11]);               // get Sourc MAC Address
+          (void*)packet[9], (void*)packet[10], (void*)packet[11]);                   // get Sourc MAC Address
 
-  ethernet_type = ntohs(*((unsigned short *)&(packet[12])));                     // get Ethernet Type
+  ethernet_type = ntohs(*((unsigned short *)&(packet[12])));                         // get Ethernet Type
 
-  if(ethernet_type == 0x0800){                                                   // ethernet_type == IP
+  if(ethernet_type == 0x0800){                                                       // ethernet_type == IP
 
       ip_version = packet[14] >> 4;
-      if((int)ip_version == 4){                                                  // ip_version == 4
+      if((int)ip_version == 4){                                                      // ip_version == 4
+          protocol = packet[23];
+          if((int)protocol == 6){                                                    // protocol == tcp
+              source_ip_long = *((long *)&(packet[26]));                             // get Source IP
+              destination_ip_long = *((long *)&(packet[30]));                        // get Destination IP
+              struct in_addr source_ip;
+              struct in_addr destination_ip;
+              source_ip.s_addr = source_ip_long;
+              destination_ip.s_addr = destination_ip_long;
 
-          source_ip_long = *((long *)&(packet[26]));                             // get Source IP
-          destination_ip_long = *((long *)&(packet[30]));                        // get Destination IP
-          struct in_addr source_ip;
-          struct in_addr destination_ip;
-          source_ip.s_addr = source_ip_long;
-          destination_ip.s_addr = destination_ip_long;
+              source_port = ntohs(*((unsigned short *)&(packet[34])));               // get Source Port
+              destination_port = ntohs(*((unsigned short *)&(packet[36])));          // get Destination Port
 
-          source_port = ntohs(*((unsigned short *)&(packet[34])));               // get Source Port
-          destination_port = ntohs(*((unsigned short *)&(packet[36])));          // get Destination Port
+              cout << "=================================================================="<< endl;
 
-          cout << "=================================================================="<< endl;
+              cout << "Packet Size is " << dec << pkthdr->len << " Byte" << endl;    // print packet size
+              cout << "Source MAC address\t\t->\t" << source_mac << endl;            // print Sourc MAC Address
+              cout << "Destination MAC address\t->\t" << destination_mac << endl;    // print Destination MAC Address
+              cout << "Source IP:Port\t\t->\t" << inet_ntoa(source_ip) << ":" << source_port << endl;                // print Source IP
+              cout << "Destination IP:Port\t\t->\t" << inet_ntoa(destination_ip)<< ":" << destination_port << endl;  // print Destination IP
 
-          cout << "Packet Size is " << dec << pkthdr->len << " Byte" << endl;    // print packet size
-          cout << "Source MAC address\t\t->\t" << source_mac << endl;            // print Sourc MAC Address
-          cout << "Destination MAC address\t->\t" << destination_mac << endl;    // print Destination MAC Address
-          cout << "Source IP:Port\t\t->\t" << inet_ntoa(source_ip) << ":" << source_port << endl;                // print Source IP
-          cout << "Destination IP:Port\t\t->\t" << inet_ntoa(destination_ip)<< ":" << destination_port << endl;  // print Destination IP
+              cout << "=================================================================="<< endl;
 
-          cout << "=================================================================="<< endl;
-
+          }
       }
 
   }
